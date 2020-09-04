e50a77a03ae6187aa42a1cf8ee59e83528aef58c
Carlos <carlosgamboa@mail.com>
2020-09-04 14:07:45 -0400

    9/4/2020 start

--- src/components/Notas/index.jsx
+++ src/components/Notas/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Citas = props => {
+const Citas = ({ titulo, fecha, texto }) => {
     return (
-        <div>
-            
+        <div className="note" >
+            <p>Titulo<span>{titulo}</span></p>
+            <p>{fecha}</p>
+            <p>Texto<span>{texto}</span></p>
+            <button type="button" >X</button>
         </div>
     )
 }
 
 Citas.propTypes = {
-
+    titulo: PropTypes.string,
+    fecha: PropTypes.number,
+    texto: PropTypes.string
 }
 
 export default Citas
