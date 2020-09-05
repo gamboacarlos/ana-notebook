2a790c3247d1b7f6f99d54d115221a6cf8ef97fd
Carlos <carlosgamboa@mail.com>
2020-09-04 23:16:08 -0400

    v1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+          <p>Hecho con <span>❤</span> para la mujer mas hermosa del mundo.</p>  
+        </footer>
+    )
+}
+
+export default Footer
